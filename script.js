82a55be1557809f1db94293f6c57d224053284cd
Артем Борисенко <riseartex@gmail.com>
2025-04-21 16:40:48 +0300

    Innitial commit

---
+++ script.js
@@ -0,0 +1,90 @@
+const books = [
+  {
+    image: "./img_1.jpg",
+    title: "Я бачу, вас цікавить пітьма",
+    author: "Ілларіон Павлюк",
+    genre: "Детектив",
+    price: 600,
+    status: "Прочитано",
+  },
+  {
+    image: "./img_2.jpg",
+    title: "Іди туди, де страшно. І отримаєш те, про що мрієш",
+    author: "Джим Ловлесс",
+    genre: "Нон-фікшн",
+    price: 280,
+    status: "Не прочитано",
+  },
+  {
+    image: "./img_3.jpg",
+    title: "Людина в пошуках справжнього сенсу",
+    author: "Віктор Франкл",
+    genre: "Нон-фікшн",
+    price: 170,
+    status: "В процесі",
+  },
+  {
+    image: "./img_4.jpg",
+    title: "Зелене світло",
+    author: "Метью Макконагі",
+    genre: "Детектив",
+    price: 420,
+    status: "Не прочитано",
+  },
+  {
+    image: "./img_5.jpg",
+    title: "Кафе на краю світу",
+    author: "Джон Стрелекі",
+    genre: "Нон-фікшн",
+    price: 200,
+    status: "В процесі",
+  },
+  {
+    image: "./img_6.jpg",
+    title: "Good Vibes, Good Life. Любов до себе — ключ до розкриття вашої величі",
+    author: "Векс Кінг",
+    genre: "Детектив",
+    price: 320,
+    status: "Не прочитано",
+  },
+  {
+    image: "./img_7.jpg",
+    title: "Емоційний інтелект",
+    author: "Деніел Гоулман",
+    genre: "Нон-фікшн",
+    price: 420,
+    status: "В процесі",
+  },
+  {
+    image: "./img_8.jpg",
+    title: "Танець недоумка",
+    author: "Ілларіон Павлюк",
+    genre: "Детектив",
+    price: 500,
+    status: "Прочитано",
+  },
+];
+
+const bookToPaste = books.map((book) => {
+  let statusClass = "";
+
+  if (book.status === "Не прочитано") {
+    statusClass = "not-read";
+  } else if (book.status === "В процесі") {
+    statusClass = "in-progress";
+  } else if (book.status === "Прочитано") {
+    statusClass = "read";
+  }
+
+  return `<div class="book">
+  <div class="image"><img src="${book.image}" alt="book"></div>
+  <h2 class="title">${book.title}</h2>
+  <p class="author">${book.author}</p>
+  <p class="price">${book.price} грн</p>
+  <p class="status ${statusClass}">${book.status}</p>
+</div>`;
+}).join("");
+
+const bookContainer = document.querySelector(".books");
+
+bookContainer.insertAdjacentHTML("afterbegin", bookToPaste);
